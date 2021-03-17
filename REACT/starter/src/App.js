11aa8d54a73f01889a49aba892c6a97334a64a29
Ken Shimauchi <kshimauchi@gmail.com>
2021-03-16 19:18:25 -0700

    fix

--- REACT/starter/src/App.js
+++ REACT/starter/src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-    
       </header>
     </div>
   );
