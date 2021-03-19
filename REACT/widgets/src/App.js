d9f9710c5d12a334684d84bc992aa8f62a432196
Ken Shimauchi <kshimauchi@gmail.com>
2021-03-18 22:16:17 -0700

    widgets: items fallback array passed down to accordion component through props, need to wire in a list

---
+++ REACT/widgets/src/App.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import Accordion from './components/accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end framework'
+  },
+  {
+    title: 'Why use React',
+    content: 'React is a favorite js among engineers'
+  },
+  {
+    title: 'How do you use React',
+    content: 'React is component based'
+  },
+];
+export default ()=> {
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
+};
