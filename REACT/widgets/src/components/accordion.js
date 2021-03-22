886925ac7f0423a9d2683b7eee8263b48cbea66d
Ken Shimauchi <kshimauchi@gmail.com>
2021-03-22 11:07:44 -0700

    useState: and templating to change className, active which expands the section

--- REACT/widgets/src/components/accordion.js
+++ REACT/widgets/src/components/accordion.js
@@ -2,8 +2,10 @@
 import React, {useState} from 'react';
 
 const Accordion = ( {items} ) => {
-    //initialize state
-    // array destructuring
+    // initialize state
+    // array destructuring shortcut for referencing
+    // activeIndex is the value and setActiveIndex is a function
+    // which changes the value
     const [activeIndex, setActiveIndex]= useState(null);
     
     //Helper functions (this could get out of hand real quick)
@@ -13,25 +15,29 @@ const Accordion = ( {items} ) => {
     };
     
     const renderedItems = items.map( (item,index) => {
+        //we want to expand selected activeIndex not have them already expanded
+        //below we remove the className name 'active' on each and we will refactor
+        //to make them active selected, that is, expanded
+        const active = index === activeIndex ? 'active' : ''
         return (
             <React.Fragment key={item.title}>
                 <div 
-                    className="title active"
+                    className={`title ${active}`}
                     onClick={()=> onTitleClick(index)}
                     //{ ()=> console.log('Titled Clicked', index)}
                 >
                 <i className="dropdown icon"></i>
                     {item.title}
                 </div>
-                <div className="content active">
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
                 </React.Fragment>
             );
             });
         return( 
-                <div className="ui styled accordion">{renderedItems}
-                <h1>{activeIndex}</h1>
+                <div className="ui styled accordion">
+                    {renderedItems}
                 </div>
             );
         };   
