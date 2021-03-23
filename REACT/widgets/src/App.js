9b705621d6a6ccb8b5384ab505f79c2e314b35b9
Ken Shimauchi <kshimauchi@gmail.com>
2021-03-23 08:11:55 -0700

    search component this is all seperate will be wired up at a later date

--- REACT/widgets/src/App.js
+++ REACT/widgets/src/App.js
@@ -1,25 +1,27 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
 import Accordion from './components/accordion';
+import Search from './components/search';
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front end framework'
-  },
-  {
-    title: 'Why use React',
-    content: 'React is a favorite js among engineers'
-  },
-  {
-    title: 'How do you use React',
-    content: 'React is component based'
-  },
-];
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front end framework'
+//   },
+//   {
+//     title: 'Why use React',
+//     content: 'React is a favorite js among engineers'
+//   },
+//   {
+//     title: 'How do you use React',
+//     content: 'React is component based'
+//   },
+// ];
 export default ()=> {
   return (
     <div>
-      <Accordion items={items} />
+      <Search />
     </div>
   );
 };
+//<Accordion items={items} />
---
+++ REACT/widgets/src/components/search.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//debouncing the search
+const Search = () => {
+
+    return(
+        <div>
+        <div className="ui form">
+            <div className="field">
+                <label>Enter Search Term</label>
+                <input className="input" />
+            </div>
+        </div>
+        </div>
+    );
+};
+export default Search;
