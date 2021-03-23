8e296286601ec70051b4e7a6063834df7bd45d19
Ken Shimauchi <kshimauchi@gmail.com>
2021-03-23 08:17:15 -0700

    setup input field to accept search term, needs the api request

--- REACT/widgets/src/components/search.js
+++ REACT/widgets/src/components/search.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 //debouncing the search
 const Search = () => {
-
+    const [term, setTerm] = useState('');
+    //request
     return(
         <div>
         <div className="ui form">
             <div className="field">
                 <label>Enter Search Term</label>
-                <input className="input" />
+                <input 
+                className="input"
+                value ={term}
+                onChange={e=> setTerm(e.target.value)}
+
+                />
             </div>
         </div>
         </div>
