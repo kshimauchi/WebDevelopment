29adf10256d9b5469c067ddf22daf3de65dbef32
Ken Shimauchi <kshimauchi@gmail.com>
2021-03-24 18:14:55 -0700

    added event handler to dropdown using useEffect hook to control the piece of state of the selected item

--- REACT/widgets/src/App.js
+++ REACT/widgets/src/App.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from 'react';
+import React, {useState} from 'react';
+
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
@@ -20,9 +21,14 @@ const options = [
 ];
 
 export default ()=> {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-        <Dropdown options={options}/>
+        <Dropdown 
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+        />
     </div>
   );
 };
