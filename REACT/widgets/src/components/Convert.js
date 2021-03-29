1309e118bbccfb79f1360e8c293973e2fe361224
Ken Shimauchi <kshimauchi@gmail.com>
2021-03-28 21:24:11 -0700

    refactored the api call, created state for translation, set the translation, called the helper method

--- REACT/widgets/src/components/Convert.js
+++ REACT/widgets/src/components/Convert.js
@@ -3,24 +3,34 @@ import React, {useState, useEffect} from 'react';
 
 
 const Convert =( {language, text} ) => {
-    //axios post, url, {} {}
+    
+    const [translated, setTranslate] = useState('');
+
+    
     useEffect( () => {
-        
-        axios.post(`${process.env.REACT_APP_API_TRANSLATE_API_URL}`,
+        //cannot directly use async inside useEffect directly
+        //Here we are calling a helper function and destructring out the {data}
+        //Post Request
+        const doTranslation = async () =>{
+           const {data} = await axios.post(`${process.env.REACT_APP_API_TRANSLATE_API_URL}`,
         {},
-        {
-            params : {
-            q:text,
-            target: language.value,
-            key: `${process.env.REACT_APP_API_TRANSLATE_KEY}`
-        },
-    }
-    );
-            
+            {
+                params : {
+                q:text,
+                target: language.value,
+                key: `${process.env.REACT_APP_API_TRANSLATE_KEY}`
+            },
+            }
+        );
+        //axios, resp data
+        setTranslate(data.data.translations[0].translatedText);
+    }; 
+        doTranslation();
+        
     },[language, text]);
     return (
         <div>
-            
+            <h1 className="ui header">{translated}</h1>
         </div>
     );
 };
