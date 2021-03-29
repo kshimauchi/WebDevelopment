7261d49743e63f4ccf9dcf99bfc04aa9e8c86f79
Ken Shimauchi <kshimauchi@gmail.com>
2021-03-29 08:48:42 -0700

    added manual routing component for navigation

--- REACT/widgets/src/App.js
+++ REACT/widgets/src/App.js
@@ -5,7 +5,7 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
-
+import Route from './routes/Routes';
 
 const items = [
   {
@@ -33,13 +33,61 @@ const options = [
     value: 'blue'
   },
 ];
-
+//Manual Route Mapping for navigation
+const showAccordion = () =>{
+  if(window.location.pathname === '/'){
+    return <Accordion items={items} />;
+  }
+};
+const showList = () =>{
+  if(window.location.pathname === '/list'){
+    return <Search />;
+  }
+};
+const showDropdown = ()=>{
+  if(window.location.pathname === '/dropdown'){
+  return <Dropdown/>;
+  }  
+};
+const showTranslate = ()=>{
+  if(window.location.pathname === '/translate'){
+    return <Translate/>;
+  }
+};
+//The inner components are a child property in Route
 export default ()=> {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div> 
-      <Translate />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search/>
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+        label="Select a Color"
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+        
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate/>
+      </Route>
+
     </div>
   );
 };
-//<Accordion items={items} />
+/*
+const showComponents = (route, component)=>{
+  return window.location.pathname === route
+  ? component
+  : null;
+};
 
+
+
+*/
