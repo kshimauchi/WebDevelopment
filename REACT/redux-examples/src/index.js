63b057bccbad35a5a01bfe51b075ef2872a87ebf
Ken Shimauchi <kshimauchi@gmail.com>
2021-04-07 14:34:28 -0700

    steps on redux-lifecycle, very basic

--- REACT/redux-examples/src/index.js
+++ REACT/redux-examples/src/index.js
@@ -2,4 +2,14 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
 
+
+//REDUX: LIFE-CYCLE
+//(1) STORE: INFO, GLOBALIZED STATE
+//(2) ACTION: ACTION CREATOR: A function that returns an object
+//(4) REDUCER:  How does your action transform you state to another state
+//(5) Dispatch: We update the globalized state depending on what action is taken
+
+
+
+
 ReactDom.render(<App/>,document.querySelector('#root'));
