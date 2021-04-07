29f6aaec5424992dfc944c10f4951dc5d11ae985
Ken Shimauchi <kshimauchi@gmail.com>
2021-04-07 14:20:25 -0700

    redux-example, first commit

---
+++ REACT/redux-examples/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      </header>
+      <div><h1>Hello</h1></div>
+    </div>
+  );
+}
+
+export default App;
