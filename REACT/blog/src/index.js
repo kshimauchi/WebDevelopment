3c7848669ddb066210abb59acc4a9aafb47164d0
Ken Shimauchi <kshimauchi@gmail.com>
2021-04-20 14:52:36 -0700

    added, component added some dummy reducer until I sort out creating the reducers

--- REACT/blog/src/index.js
+++ REACT/blog/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+import './index.css';
+import App from './components/App';
+import reducers from './reducers';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={createStore(reducers)}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.querySelector('#root')
 );
 
---
+++ REACT/blog/src/reducers/index.js
@@ -0,0 +1,5 @@
+import {combineReducers} from 'redux';
+
+export default combineReducers({
+    placeholder: ()=> 'to be implemented'
+});
