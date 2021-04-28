dc414036e67350386c368d447b26548b2cff3f1c
Ken Shimauchi <kshimauchi@gmail.com>
2021-04-28 10:39:51 -0700

    memoized action fetchUsers so that only one call is made for every single user, previous code was calling users multiple times for every post

--- REACT/blog/src/actions/index.js
+++ REACT/blog/src/actions/index.js
@@ -1,5 +1,7 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+//We are going to memoize, an action creator
 export const fetchPosts = ()=> async dispatch => {
     
     const response = await jsonPlaceholder.get('/posts');
@@ -7,9 +9,29 @@ export const fetchPosts = ()=> async dispatch => {
         dispatch({type: 'FETCH_POSTS', payload: response.data});
 };
 
-export const fetchUser = (id)=> async dispatch => {
-    
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-
-        dispatch({type: 'FETCH_USER', payload: response.data});
+export const fetchUser = id => dispatch => {
+   _fetchUser(id, dispatch);
 };
+
+//do not play with this
+const _fetchUser = _.memoize(async (id, dispatch)=> {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+        dispatch({type: 'FETCH_USER', payload: response.data});
+});
+
+/*
+From previous, which doesn't work 
+if we memoize the outer function this will still return
+a ton of requests, this expected behavior every time we call the
+function of inner function the first time redux thunk will continue to 
+call the function with dispatch, and the network request
+
+if we memoize the inner fuction this will also result in the 
+same problem, why? redux thunk will call it one time but this is not the
+case the same number of requests.
+
+Everytime we use the fetchUser we create a new version of the 
+inner function so, the action creator will continue to invoke
+
+So the real way, to handle this case is we need to declared above
+*/
--- REACT/blog/src/components/UserHeader.js
+++ REACT/blog/src/components/UserHeader.js
@@ -2,12 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchUser } from '../actions';
 
-// We should use less on this we want to show one user
-// (1) we could use renderList or
-// (2) move the find, to the mapStateToProps method
-//  removed the findById inside the render method and extracted it to 
-//  mapStateToProps, refactored the render method, for a single user
-//  on the props, 
+
 class UserHeader extends React.Component {
     
     componentDidMount(){
@@ -24,12 +19,6 @@ class UserHeader extends React.Component {
         );
     };
 }
-// we are going to alter this to make it reusable,
-// sometimes mapStateToProps and connect are seperate files
-// extracting the computation off the state
-// added ownProps argument, which has a reference to props,
-// redux state still on the first argument
-
 const mapStateToProps= (state, ownProps )=>{
     return { user : state.users.find(user => user.id === ownProps.userId) };
 };
