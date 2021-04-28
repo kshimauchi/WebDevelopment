9e71e0079f82096086c6aa4d04538607fa858cf2
Ken Shimauchi <kshimauchi@gmail.com>
2021-04-28 16:18:44 -0700

    refactored the fetchPostsAndUsers action and used lodash chaining, see comments

--- REACT/blog/src/actions/index.js
+++ REACT/blog/src/actions/index.js
@@ -4,30 +4,24 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 export const fetchPostsAndUsers = ()=> async (dispatch, getState) => {
 
     await dispatch(fetchPosts()); 
-    const userIds = _.uniq(_.map(getState().posts, 'userId'))
-                    
-    userIds.forEach(id=> dispatch(fetchUser(id)));
-  
+                                                // Lodash :
+    _.chain(getState().posts)                   // chain allows to chain on additional function on a collection of data
+        .map('userId')                          // gives us the map(arg1) whatever object we are chaining over, we want userId
+        .uniq()                                 // this result, we can chain onto, unique since we want unique userIds,
+        .forEach(id=> dispatch(fetchUser(id)))  // we call the function for every uniq Id
+        .value();                               // we simply call value(), so we can execute
 };
 
-//We are going to memoize, an action creator
 export const fetchPosts = ()=> async dispatch => {
     
     const response = await jsonPlaceholder.get('/posts');
    
         dispatch({type: 'FETCH_POSTS', payload: response.data});
 };
-// Original, ton of requests
+
 export const fetchUser = id => async dispatch =>{
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({type : 'FETCH_USER', payload: response.data });
 };
 
-// ** ** Memoized version one approach will be workig on a second approach ** **
-// import _ from 'lodash';
-// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-//     const _fetchUser = _.memoize(async (id, dispatch)=> {
-//         const response = await jsonPlaceholder.get(`/users/${id}`);
-//         dispatch({type: 'FETCH_USER', payload: response.data});
-// });
