5ab51bd11d780bfbbc82a446289d4ac8c44dcb38
Ken Shimauchi <kshimauchi@gmail.com>
2021-04-30 15:14:30 -0700

    scaffold, stream crud components with list and scaffold routing using react-route-dom

--- REACT/streams/client/src/components/App.js
+++ REACT/streams/client/src/components/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import  { BrowserRouter, Route , Link} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 
 const App = ()=> {
   return (
     <div>
-      <h1>App</h1>
+      <BrowserRouter>
+      <div>
+        <Route path="/" exact component={StreamList}/> 
+        <Route path="/streams/new" component={StreamCreate} />
+        <Route path="/streams/edit" component={StreamEdit}/>
+        <Route path="/streams/delete" component={StreamDelete}/>
+        <Route path="/streams/show" component={StreamShow}/>
+      </div>  
+      </BrowserRouter>
     </div>
   );
-}
+};
 export default App;
