859d9f22a5449b312e4e8d9e6d0b95f9ee7b8db9
Ken Shimauchi <kshimauchi@gmail.com>
2021-05-02 13:23:43 -0700

    added renderInput, form input on the streams/new...create route,formProps is handling the text of the input on the ui currently

--- REACT/streams/client/src/components/streams/StreamCreate.js
+++ REACT/streams/client/src/components/streams/StreamCreate.js
@@ -1,6 +1,38 @@
 import React from 'react';
+import { Field, reduxForm} from 'redux-form';
 
-const StreamCreate = ()=>{
-    return <div>StreamCreate</div>;
-};
-export default StreamCreate;
+//(1) converting to class component
+//(2) Field, reduxForm
+/*
+Field, suppose to be react component
+reduxForm, is a function similar to the connect function
+*/
+//(3) wiring 
+class StreamCreate extends React.Component{
+    renderInput(formProps){
+        //needs to be a controlled element
+        /*
+                onChange={formProps.input.onChange}
+                value={formProps.input.value}
+        */
+        //console.log(formProps);
+        return(
+            //adds them to input element as props using jsx
+            <input {...formProps.input} />
+        );
+    }
+    render() {
+        console.log(this.props);
+        return(
+            <form>
+                <Field name="title" component={this.renderInput} />
+                <Field name="description" component={this.renderInput} />
+
+            </form>
+        ); 
+    }
+}
+//receives a single object
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
