0ceefd55291b1444d614b10c27d6f61d49d71a72
Ken Shimauchi <kshimauchi@gmail.com>
2021-05-02 15:37:41 -0700

    changed the css style,to ui to create errors on form. but also had to fix renderInput had to be changed to an error function since it used this, which had no context

--- REACT/streams/client/src/components/streams/StreamCreate.js
+++ REACT/streams/client/src/components/streams/StreamCreate.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
-import '../../css/streams/streamStyles.css';
+//import '../../css/streams/streamStyles.css';
 
 class StreamCreate extends React.Component{
    
-    renderInput({input,label, meta}){
+    // <div className="error">{meta.error}</div>
+    // semantic ui disables errors by default, which we can add error on the className for the
+    // form
+    renderError ({error, touched}){
+       if(touched && error){
+        return(
+            <div className="ui error message">
+                <div className="header">
+                    {error}
+                </div>
+            </div>
+        );
+       }
+   }
+    renderInput = ( {input,label, meta })=> {
         //console.log(meta);
+        //context issue using this, undefined have to change to ()=>
         return(     
             <div className="field">
                 <label>{label}</label>
-                <input {...input} />
-                <div className="error">{meta.error}</div>
+                <input {...input} autoComplete="off"/>
+                {this.renderError(meta)}
             </div>
         );
     }
@@ -22,7 +37,7 @@ class StreamCreate extends React.Component{
         return(
             <form 
                 onSubmit={this.props.handleSubmit(this.onSubmit)} 
-                className="ui form">
+                className="ui form error">
 
                 <Field 
                     name="title" 
