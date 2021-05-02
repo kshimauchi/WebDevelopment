b7fd4023bb7c8147bb38edf3f2e3dda5a0453050
Ken Shimauchi <kshimauchi@gmail.com>
2021-05-02 13:33:01 -0700

    styled input, needs a little work

--- REACT/streams/client/src/components/streams/StreamCreate.js
+++ REACT/streams/client/src/components/streams/StreamCreate.js
@@ -1,34 +1,29 @@
 import React from 'react';
 import { Field, reduxForm} from 'redux-form';
 
-//(1) converting to class component
-//(2) Field, reduxForm
-/*
-Field, suppose to be react component
-reduxForm, is a function similar to the connect function
-*/
-//(3) wiring 
 class StreamCreate extends React.Component{
-    renderInput({input}){
-
-        //console.log(formProps);
-        return(
-          
-            <input {...input} />
+    renderInput({input,label}){
+        
+        return(     
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
         );
     }
     render() {
         console.log(this.props);
         return(
-            <form>
-                <Field name="title" component={this.renderInput} />
-                <Field name="description" component={this.renderInput} />
+            <form className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter Title" />
+                <Field 
+                    name="description" 
+                    component={this.renderInput} 
+                    label="Enter Description" />
 
             </form>
         ); 
     }
 }
-//receives a single object
-export default reduxForm({
-    form: 'streamCreate'
-})(StreamCreate);
+
+export default reduxForm({form: 'streamCreate' })(StreamCreate);
