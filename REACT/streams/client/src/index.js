a88018738c859fe5ee7fb3dd977adbaa420e92d4
Ken Shimauchi <kshimauchi@gmail.com>
2021-05-02 12:22:17 -0700

    installed redux-form, added to reducers index as formReducer

--- REACT/streams/client/src/index.js
+++ REACT/streams/client/src/index.js
@@ -6,6 +6,10 @@ import {createStore, applyMiddleware, compose} from 'redux';
 
 import reducers from './reducers';
 // dev tools, need to add the middleware
+// can use debug_sessions by using the
+// \?debug_sessions=random string character
+// we can use debug sessions to create different check points
+// redux form: https://redux-form.com/8.3.0/
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware()));
