b7e760543ba39e6782386c63571f8c4c9ec4fcea
Ken Shimauchi <kshimauchi@gmail.com>
2021-05-13 19:40:47 -0700

    streamForm component is generalized form for the streamCreate functionality, this will not compile right now

--- REACT/streams/client/src/components/streams/StreamEdit.js
+++ REACT/streams/client/src/components/streams/StreamEdit.js
@@ -3,15 +3,10 @@ import {connect} from 'react-redux';
 import {fetchStream} from '../../actions';
 
 class StreamEdit extends React.Component{
-    //Same properties, but we have redux state 
-    //console.log(props)::: history, location and match
-    //match is the params of the name we gave it in routing
-    //we care about that the streams object: redux/state store
-    //using props id we can find the one we want,
+
     componentDidMount(){
         this.props.fetchStream(this.props.match.params.id);
     }
-
     render(){
         if(!this.props.stream){
             return <div>Loading...</div>
@@ -19,9 +14,9 @@ class StreamEdit extends React.Component{
         return <div>{this.props.stream.title}</div>;
     };
 };
-//Map state to props is now inter connected
+
 const mapStateToProps = (state,ownProps)=> {
-    console.log(ownProps);
+    
     return {stream : state.streams[ownProps.match.params.id]};
 };
-export default connect(mapStateToProps, {fetchStream } )(StreamEdit);
+export default connect(mapStateToProps, { fetchStream } )(StreamEdit);
