ba8fdd38e551b9a19527706500d6a650fb6322b6
Ken Shimauchi <kshimauchi@gmail.com>
2021-05-14 16:53:25 -0700

    building modal,for delete stream

--- REACT/streams/client/src/components/Modal.js
+++ REACT/streams/client/src/components/Modal.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-//want to nest this to the body element
+import history from '../history';
+
 const Modal = (props) =>{
     
     return ReactDom.createPortal(
-        <div className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
-                portal modal
+        <div 
+            onClick={()=>history.push('/') } 
+            className="ui dimmer modals visible active"
+            >
+            
+            <div 
+            onClick={(e) => e.stopPropagation() } 
+            className="ui standard modal visible active"
+            >
+                
+            <div className="header">Delete Stream</div>
+            
+            <div className="content">
+                Are you sue you want to delete this stream
             </div>
+
+            <div className="actions">
+                <button className="ui primary button">delete</button>
+                <button className="ui button">Cancel</button>
+            </div>
+        
+        </div>
         </div>,
         document.querySelector('#modal')
     );
--- REACT/streams/client/src/components/streams/StreamDelete.js
+++ REACT/streams/client/src/components/streams/StreamDelete.js
@@ -7,7 +7,6 @@ const StreamDelete = ()=>{
             StreamDelete
             <Modal />
         </div>
-        
     );
 };
 export default StreamDelete;
