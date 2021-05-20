a05f051684344211917a8fd32562fe7152dccc41
Ken Shimauchi <kshimauchi@gmail.com>
2021-05-20 13:30:12 -0700

    language selector

--- REACT/translate/src/components/App.js
+++ REACT/translate/src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
+    state = { language: 'english' };
+    onLanguageChange = language => {
+        
+        this.setState({ language });
 
+    }
     render() {
-        return <div className="ui container">App</div>;
+        return (
+            <div className="ui container">
+                <div>
+                    Select a language :
+                     <i className="flag us" onClick={ ()=> this.onLanguageChange('english')}/>
+                     <i className="flag jp" onClick={ ()=> this.onLanguageChange('japanese')}/>
+                </div>
+                {this.state.language}
+                 
+            </div>
+        );
     }
 }
 export default App;
