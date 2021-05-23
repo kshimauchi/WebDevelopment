198630eb065e97d43df11ff0e8ad9d72b26c8125
Ken Shimauchi <kshimauchi@gmail.com>
2021-05-23 13:10:22 -0700

    created gets/posts to comments/posts, postman manual check

---
+++ REACT_MICROSERVICE/blog/comments/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+
+const app = express();
+app.use(express.json());
+
+// we are looking for all posts by user
+// 'a3342fdsa' 
+// {id: 'kfjldajkjfd12', content:'infomative'}
+// in memory storage only there until server reboots
+const commentsByPostId = {};
+
+
+app.get('/posts/:id/comments', (req, res) =>  {
+    res.send(commentsByPostId[req.params.id] || [])
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+    const comments = commentsByPostId[req.params.id] || [];
+
+    comments.push({ id: commentId, content });
+    
+    commentsByPostId[req.params.id] = comments;
+    
+    res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+    console.log('Listening on 4001');
+});
+/*
+GET all posts by user, status 200
+localhost:4001/posts/123/comments
+[
+    {
+        "id": "409e8dfe",
+        "content": "this is a comment"
+    },
+    {
+        "id": "9d33756f",
+        "content": "this a second comment"
+    }
+]
+POST userId could be anything just an example
+localhost:4001/posts/123/comments
+{
+    "content": "this a second comment"
+}
+*/
--- REACT_MICROSERVICE/blog/posts/index.js
+++ REACT_MICROSERVICE/blog/posts/index.js
@@ -1,18 +1,73 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
 
 
 const app = express();
 
+// app.use(bodyParser.json()); as of express 4.16+ we can replace this line
+app.use(express.json());
+
+//object storage for the time being
+const posts = {};
+
+// Route Handlers, just in memory just an example
+// Get all posts
 app.get('/posts', (req, res) => {
-    
+    res.send(posts);
 });
 
-app.post('/posts', (req, res) => { });
-
+//Create a resource 
+app.post('/posts', (req, res) => {
+    
+    const id = randomBytes(4).toString('hex');
+    
+    const { title } = req.body;
+    
+    posts[id] = {
+        id, title
+    };
+    //sends a response for a new posts
+    res.status(201).send(posts[id]);
+});
 
 app.listen(4000, () => {
     console.log('Listening on Port 4000');
 });
 
+/*
+send post request, to localhost:4000/posts with body below 
+should get a response manual
 
+POST
+201 status with create
+    {
+        "title" : "first post"
+    }
+201 status but, notice the response only has id
+    {
+        "wrong" : "second post"
+    }
+GET    
+localhost:4000/posts
+we recieve the entire object, blob
+
+{
+    "66b45810": {
+        "id": "66b45810",
+        "title": "second post"
+    },
+    "7bb6b061": {
+        "id": "7bb6b061"
+    },
+    "86c0dd5e": {
+        "id": "86c0dd5e",
+        "title": "second post"
+    },
+    "bd8ad1ae": {
+        "id": "bd8ad1ae"
+    }
+}
+
+*/
 
