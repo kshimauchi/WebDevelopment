b49dbd88f247e717e66d3e0d0cc37f3a32f07a04
Ken Shimauchi <kshimauchi@gmail.com>
2021-05-24 18:07:58 -0700

    queryService and refactor

--- REACT_MICROSERVICE/blog/event-bus/index.js
+++ REACT_MICROSERVICE/blog/event-bus/index.js
@@ -1,23 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
-
 app.use(express.json());
 
-app.post('/event', (req, res) => {
-    
-    const event = req.body;
+app.post("/events", (req, res) => {
+  const event = req.body;
 
-    // series of posts to running services
-    // one of these could fail and crash
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
-   
-    res.send({ status: 'OK' });
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  res.send({ status: "OK" });
 });
+
 app.listen(4005, () => {
-    console.log('Listening for Events on 4005');
+  console.log("Listening on 4005");
 });
