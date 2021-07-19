729eaf97b1816e5861c4ad3399e3f4879cec34a8
Ken Shimauchi <kshimauchi@gmail.com>
2021-07-19 10:51:10 -0700

    extracted index logic to app, and using app as configuration, for testing purposes

--- REACT_MICROSERVICE/ticketing/auth/src/index.ts
+++ REACT_MICROSERVICE/ticketing/auth/src/index.ts
@@ -1,41 +1,6 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-//behind ingress engine and express needs to trust this
-app.set('trust proxy', true);
-app.use(json());
-
-app.use(
-    cookieSession({
-        //JWT Token is already encrypted
-        //and in so doing would make encrypting and decrypting
-        //more complicated if multiple languages are used on backend
-        signed: false,
-        secure: true
-    })
-);
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", async (req, res) => {
-    throw new NotFoundError();
-});
-
-app.use(errorHandler);
-
+import { app } from './app';
 //capture jwt key error if not defined
 if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
