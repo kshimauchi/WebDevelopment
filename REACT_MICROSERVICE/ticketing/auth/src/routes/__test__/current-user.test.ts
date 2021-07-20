db5dd825f4a884641f4624db19d3fa96f9ab7d36
Ken Shimauchi <kshimauchi@gmail.com>
2021-07-20 11:39:48 -0700

    current-user test on not authenticated

--- REACT_MICROSERVICE/ticketing/auth/src/routes/__test__/current-user.test.ts
+++ REACT_MICROSERVICE/ticketing/auth/src/routes/__test__/current-user.test.ts
@@ -1,8 +1,9 @@
 import request from 'supertest';
 import { app } from '../../app';
 
+
+//global function defined in test/setup
 it('responds with details about current user', async () => {
-    //global function defined in test/setup
     const cookie = await global.signin();
 
     const response = await request(app)
@@ -12,6 +13,17 @@ it('responds with details about current user', async () => {
         .expect(200);
     expect(response.body.currentUser.email).toEqual(
         'test@test.com'
+    )
+});
+//given a user is not authenticated, test null
+it('responds with null if not authenticated', async () => {
+    //global function defined in test/setup
+    const response = await request(app)
+        .get('/api/users/currentuser')
+        .send()
+        .expect(200);
+    expect(response.body.currentUser.email).toEqual(
+        null
     )
 });
 //refactoring auth to a globally signin function
