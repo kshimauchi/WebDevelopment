99817c08de8fd3c0623fa6a334eb53380f645449
Ken Shimauchi <kshimauchi@gmail.com>
2021-07-20 19:41:48 -0700

    setup event handlers on signup ui

--- REACT_MICROSERVICE/ticketing/client/pages/auth/signup.js
+++ REACT_MICROSERVICE/ticketing/client/pages/auth/signup.js
@@ -1,15 +1,30 @@
-
+import { useState } from 'react';
 export default function authSignup(){
+    const [email, setEmail] = useState('');
+    const [password, setPasword] = useState('');
+
+    const onSubmit = (event)=> {
+        event.preventDefault();
+        console.log(email, password);
+    }
     return (
-        <form>
+        <form onSubmit={ onSubmit} >
             <h1>Sign Up</h1 >
             <div className="form-group">
                 <label>Email Address</label>    
-                <input className="form-control"/>
+                <input
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    className="form-control"
+                />
             </div>
             <div className="form-group">
                 <label>Password</label>
-                <input type="password" className="form-control"/>
+                <input type="password"
+                    value={password}
+                    onChange={e=> setPasword(e.target.value)}
+                    className="form-control"
+                />
             </div>
             <button className="btn btn-primary">Sign up</button>
         </form>
