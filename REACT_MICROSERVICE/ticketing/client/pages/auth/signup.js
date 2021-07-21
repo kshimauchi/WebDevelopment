f28efbb9fd7714973f248abe0fba602e8dc46fcb
Ken Shimauchi <kshimauchi@gmail.com>
2021-07-20 20:38:06 -0700

    signup component with error messaging

--- REACT_MICROSERVICE/ticketing/client/pages/auth/signup.js
+++ REACT_MICROSERVICE/ticketing/client/pages/auth/signup.js
@@ -4,14 +4,21 @@ import axios from 'axios';
 export default function authSignup(){
     const [email, setEmail] = useState('');
     const [password, setPasword] = useState('');
-
+    const [errors, setErrors] = useState([]);
+    
+    
     const onSubmit = async (event) => {
         event.preventDefault();
         
+    try {
         const response = await axios.post('/api/users/signup', {
-            email, password
-        });
-        console.log(response.data);
+                email, password
+            });
+            console.log(response.data);
+    } catch (err) {
+        //doh 
+            setErrors(err.response.data.errors);
+    }
     };
     return (
         <form onSubmit={ onSubmit} >
@@ -24,18 +31,33 @@ export default function authSignup(){
                     className="form-control"
                 />
             </div>
+            
             <div className="form-group">
                 <label>Password</label>
-                <input type="password"
+                <input
+                    type="password"
                     value={password}
-                    onChange={e=> setPasword(e.target.value)}
+                    onChange={e => setPasword(e.target.value)}
                     className="form-control"
                 />
             </div>
+           
+            {errors.length > 0 &&
+                (<div className="alert alert-danger">
+                <h4>...An Error is in Progress!</h4>
+                <ul className="my-0">
+                    {errors.map(err =>
+                        <li key={err.message}>
+                            {err.message}
+                        </li>)}
+                </ul>
+            </div>
+           )}
             <button className="btn btn-primary">Sign up</button>
         </form>
         
     );
 };
-
+// TODO: need logic to show or not show the alert
+// TODO: Extract the Errors logic for usability
 //auth/signup
