b7f8bc101e7ac529d1d988c81f46ce25a40596e4
Ken Shimauchi <kshimauchi@gmail.com>
2021-07-20 19:29:35 -0700

    basic signup ui

---
+++ REACT_MICROSERVICE/ticketing/client/pages/auth/signup.js
@@ -0,0 +1,20 @@
+
+export default function authSignup(){
+    return (
+        <form>
+            <h1>Sign Up</h1 >
+            <div className="form-group">
+                <label>Email Address</label>    
+                <input className="form-control"/>
+            </div>
+            <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control"/>
+            </div>
+            <button className="btn btn-primary">Sign up</button>
+        </form>
+        
+    );
+};
+
+//auth/signup
