c51ca6f870ed82ee1e0030f7591cbbb6b303459e
Ken Shimauchi <kshimauchi@gmail.com>
2021-07-29 11:01:27 -0700

    buildclient extracted for clarity

--- REACT_MICROSERVICE/ticketing/client/pages/index.js
+++ REACT_MICROSERVICE/ticketing/client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   // console.log(currentUser);
@@ -7,33 +7,12 @@ const LandingPage = ({ currentUser }) => {
 
   return <h1>Landing Page</h1>;
 };
+//build client then make request, buildClient in api
+LandingPage.getInitialProps = async context => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    // we are on the server!
-    // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
-
-    return data;
-  } else {
-    // we are on the browser!
-    // requests can be made with a base url of ''
-    const { data } = await axios.get('/api/users/currentuser');
-
-    return data;
-  }
-  return {};
+  return data;
 };
-//How to make a request to ingress nginx, from inside
-//a cluster.
 
 export default LandingPage;
-/*
-next js has some issues with file change detection
-within a docker container
-*/
