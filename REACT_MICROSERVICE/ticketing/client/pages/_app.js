0d74ca7963b28d9bf9c20d1a0aa04a6ebf7c6e1e
Ken Shimauchi <kshimauchi@gmail.com>
2021-08-01 12:12:26 -0700

    restructuring

--- REACT_MICROSERVICE/ticketing/client/pages/_app.js
+++ REACT_MICROSERVICE/ticketing/client/pages/_app.js
@@ -12,18 +12,13 @@ const AppComponent = ({ Component, pageProps, currentUser })=> {
     </div>
   );
 };
-// nextJS will have to be altered here to fetch data to wrap up pages
-// but the arguments are different inside a page we use (context) === {req, res}
-// In a Page Component, the req, is nested inside ctx
-// This context here is different than context,  'AppTree', 'Component', 'router', 'ctx' 
-// and we want to pass ctx to the buildClient
+
 AppComponent.getInitialProps = async (appContext) => {
   
   const client = buildClient(appContext.ctx);
   
   const { data } = await client.get('/api/users/currentuser');
-  
-  //case of fail its assigned as null
+
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
       pageProps = await appContext.Component.getInitialProps(appContext.ctx);
@@ -34,6 +29,5 @@ AppComponent.getInitialProps = async (appContext) => {
     ...data
   } 
 };
-// want to extract the currentuser here so we will be extracting, the getInitialProps here
-// so we can pass the currentuser as a prop to the index, and signin
+
 export default AppComponent;
--- REACT_MICROSERVICE/ticketing/client/pages/auth/signout.js
+++ REACT_MICROSERVICE/ticketing/client/pages/auth/signout.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import Router from 'next/router';
 
 import useRequest from '../../hooks/use-request';
+
 //request to signout must come from inside a component
 //not from inside getInitialProps, because that is on the server
 //server is not aware of the cookie, therefore its within the component
