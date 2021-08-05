a3d7f6b1b1378b05d3b4424aaec732741cf38260
Ken Shimauchi <kshimauchi@gmail.com>
2021-08-04 22:50:55 -0700

    created some route test,should create some negatives later

--- REACT_MICROSERVICE/ticketing/tickets/src/app.ts
+++ REACT_MICROSERVICE/ticketing/tickets/src/app.ts
@@ -3,8 +3,8 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
-import { errorHandler, NotFoundError } from "@ticket-share/common";
-
+import { errorHandler, NotFoundError, currentUser } from "@ticket-share/common";
+import { createTicketRouter } from './routes/new';
 
 //Configures app
 const app = express();
@@ -17,12 +17,12 @@ app.use(
         //JWT Token is already encrypted
         //and in so doing would make encrypting and decrypting
         //more complicated if multiple languages are used on backend
-
         signed: false,
         secure: process.env.NODE_ENV !== 'test'
     })
 );
-
+app.use(currentUser);
+app.use(createTicketRouter);
 
 app.all("*", async (req, res) => {
     throw new NotFoundError();
--- REACT_MICROSERVICE/ticketing/tickets/src/routes/__test__/new.test.ts
+++ REACT_MICROSERVICE/ticketing/tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,8 @@
 import request from 'supertest';
 import { app } from '../../app';
-//Execute dir: tickets, npm run test
+
+
+//TODO tickets, ( npm run test )
 
 //Request to Route Handle
 it('has a route handler listening to /api/tickets for post request', async () => {
@@ -9,9 +11,26 @@ it('has a route handler listening to /api/tickets for post request', async () =>
         .send({});
     expect(resp.status).not.toEqual(404);
 });
-//
+//Simulate request without being signed in
 it('can only be accessed if the user is signed in', async () => {
-    
+    const response = await request(app)
+        .post('/api/tickets')
+        .send({})
+        .expect(401);
+    //expect(response.status).toEqual(401);
+});
+
+it('returns a status other 401 if the user is signed in', async () => {
+    //we do not to have inter-service dependency so we do
+    //not want to reach inside the auth service
+    //we need a cookie of sort
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({})
+    console.log(response.status);
+    expect(response.status).not.toEqual(401);
+
 });
 //
 it('returns an error if an invalid title is provided', async () => {
