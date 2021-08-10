30920a46d688986d6b082c623c41beeee10ca854
Ken Shimauchi <kshimauchi@gmail.com>
2021-08-10 16:10:50 -0700

    update tickets tests

--- REACT_MICROSERVICE/ticketing/tickets/src/routes/__test__/update.test.ts
+++ REACT_MICROSERVICE/ticketing/tickets/src/routes/__test__/update.test.ts
@@ -7,8 +7,9 @@ import mongoose from 'mongoose';
 update test: 
 Case When:
 id: dne
-user: not auth, just build so no sign on
 user: does not own the ticket
+user: not auth, just build so no sign on
+
 */
 
 it('returns 404 if the provided id does not exist', async () => {
@@ -60,12 +61,65 @@ it('returns 401 if the user does not own the ticket', async () => {
     //could verify the ticket title and price
 });
 
-it('returns 400 if the user provides an invalid title', async () => {
+it('returns 400 if the user provides an invalid title or price', async () => {
+    //same user
+    const cookie = global.signin();
     
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'ticket',
+            price: 20
+        });
+    
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: '',
+            price: 20
+        })
+        .expect(400);
+    
+
+       await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'fdafdas',
+            price: -20
+        })
+        .expect(400);
 });
 
 it('updates the ticket provided valid inputs', async () => {
+    const cookie = global.signin();
     
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'ticket',
+            price: 20
+        });
+    
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'new title',
+            price: 100
+        })
+        .expect(200);
+    
+    //fetch ticket check update
+    const ticketResponse = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send();
+    
+    expect(ticketResponse.body.title).toEqual('new title');
+    expect(ticketResponse.body.price).toEqual(100);
 });
 
 
--- REACT_MICROSERVICE/ticketing/tickets/src/routes/update.ts
+++ REACT_MICROSERVICE/ticketing/tickets/src/routes/update.ts
@@ -9,18 +9,43 @@ import {
 
 import { Ticket } from '../models/ticket';
 
+
 const router = express.Router();
 
-router.put('/api/tickets/:id', requireAuth, async (req: Request, res: Response) => {
+//TODO: clean section up for clarity
+router.put('/api/tickets/:id', requireAuth,
+    [
+        body('title')
+            .not()
+            .isEmpty()
+            .withMessage('Title is required'),
+        body('price')
+            .isFloat({ gt: 0 })
+            .withMessage('Price must be provided and greater than 0')
+        
+    ],
+    validateRequest,
+   
+    async (req: Request, res: Response) => {
+    
     const ticket = await Ticket.findById(req.params.id);
     
     if (!ticket) {
         throw new NotFoundError();
     }
+    
     // fail by commenting this out  expected 401 "Unauthorized", got 200 "OK"
     if (ticket.userId !== req.currentUser!.id) {
         throw new NotAuthorizedError();
     }
+    
+        ticket.set({
+            title: req.body.title,
+            price: req.body.price
+        });
+
+        await ticket.save();
+    
     res.send(ticket);
 });
 
