64ec5363c355b9af10b8150fab06c9a62482053b
Ken Shimauchi <kshimauchi@gmail.com>
2021-08-13 11:08:00 -0700

    sub project test driving nats streaming service

---
+++ REACT_MICROSERVICE/ticketing/nats-test/src/publisher.ts
@@ -0,0 +1,11 @@
+import nats from 'node-nats-streaming';
+
+
+const stan = nats.connect('ticketing', 'abc', {
+    url: 'http://localhost:4222'
+});
+
+// library uses callbacks
+stan.on('connect', () => {
+    console.log('Publisher connected to NATS !!!!');
+});
