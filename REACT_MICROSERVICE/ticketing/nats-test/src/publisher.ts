b1434db3a0dfbf5eaf81d5af833196b3b77609aa
Ken Shimauchi <kshimauchi@gmail.com>
2021-08-13 12:27:33 -0700

    test pub

--- REACT_MICROSERVICE/ticketing/nats-test/src/publisher.ts
+++ REACT_MICROSERVICE/ticketing/nats-test/src/publisher.ts
@@ -1,6 +1,7 @@
 import nats from 'node-nats-streaming';
 
-
+// (1) Simple port forwarding solution
+// (2) nats-pod publish: npm run publish
 const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222'
 });
@@ -8,4 +9,15 @@ const stan = nats.connect('ticketing', 'abc', {
 // library uses callbacks
 stan.on('connect', () => {
     console.log('Publisher connected to NATS !!!!');
+
+    //Share formatting is JSON
+    const data = JSON.stringify({
+        id: '001',
+        title: 'concertTicket',
+        price: 20
+    });
+
+    stan.publish('ticket: created', data, () => {
+        console.log('Event published');
+    });
 });
