ba150564f83e6ab07da4a823cc62028a27c57192
Ken Shimauchi <kshimauchi@gmail.com>
2021-08-25 18:56:13 -0700

    next-security headers added

--- REACT_MICROSERVICE/ticketing/client/next.config.js
+++ REACT_MICROSERVICE/ticketing/client/next.config.js
@@ -1,8 +1,13 @@
+const { createSecureHeaders } = require("next-secure-headers");
+
 module.exports = {
     webpackDevMiddleware: config => {
         config.watchOptions.poll = 300;
         return config;
-    }
+    },
+    async headers() {
+        return [{ source: "/(.*)", headers: createSecureHeaders() }];
+    },
 };
 //poll all files every 300s
 //also sometimes you may want to kill the pod using kubectl for file
