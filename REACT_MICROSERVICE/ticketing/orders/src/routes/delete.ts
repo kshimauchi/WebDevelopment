15387074c95a3569a549b98c6cb0995a8f18bf8f
Ken Shimauchi <kshimauchi@gmail.com>
2021-08-31 15:14:07 -0700

    delete route is more like update

--- REACT_MICROSERVICE/ticketing/orders/src/routes/delete.ts
+++ REACT_MICROSERVICE/ticketing/orders/src/routes/delete.ts
@@ -1,9 +1,28 @@
 import  express, {Request, Response} from 'express';
+import  { NotAuthorizedError, requireAuth,NotFoundError } from '@ticket-share/common';
+import {Order, OrderStatus} from '../models/order';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async(req: Request, res: Response)=>{
+// Change of status is not really a delete but patch
+router.delete('/api/orders/:orderId',
+    requireAuth, 
+    async(req: Request, res: Response)=>{
 
-    res.send({});
+    const { orderId }= req.params;
+
+    const order = await Order.findById(orderId);
+
+    if(!order){
+        throw new NotFoundError();
+    }
+    if(order.userId !== req.currentUser!.id){
+        throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+
+    await order.save();
+
+    res.status(204).send(order);
 });
 export {router as deleteOrderRouter};
