10c1b5421b0f9ca03715f92872b5e8695888e4d7
Ken Shimauchi <kshimauchi@gmail.com>
2021-09-02 21:40:12 -0700

    ticketcreated/ticketupdated listeners

--- REACT_MICROSERVICE/ticketing/orders/src/models/ticket.ts
+++ REACT_MICROSERVICE/ticketing/orders/src/models/ticket.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import { Order, OrderStatus } from './order';
 
 interface TicketAttrs {
+  id: string;
   title: string;
   price: number;
 }
@@ -39,7 +40,12 @@ const ticketSchema = new mongoose.Schema(
 );
 // statics allows us access to the overall collection
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-  return new Ticket(attrs);
+  
+  return new Ticket({
+    _id: attrs.id,
+    title: attrs.title,
+    price: attrs.price,
+  });
 };
     // Make sure that this ticket has not already reserved
     // Run query to look at all orders.  Find an order where the ticket
