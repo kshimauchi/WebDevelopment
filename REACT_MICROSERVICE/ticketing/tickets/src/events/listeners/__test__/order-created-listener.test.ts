9b4d4e8895fe41004340875aff7f8becba6bed84
Ken Shimauchi <kshimauchi@gmail.com>
2021-09-10 20:55:59 -0700

    creating instances of created/cancelled listeners

--- REACT_MICROSERVICE/ticketing/tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ REACT_MICROSERVICE/ticketing/tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -51,4 +51,17 @@ it('it acks the message', async()=>{
     await listener.onMessage(data, msg);
 
     expect(msg.ack).toHaveBeenCalled();
-})
+});
+
+it('publishes a ticket updated event', async()=>{
+    const { listener, ticket, data, msg } = await setup();
+
+    await listener.onMessage(data, msg);
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
+    //@ts-ignore
+    //(natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+    //const ticketId = natsWrapper.client.publish.mock.calls[0][1];
+    const ticketUpdatedData = JSON.parse( (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]);
+    expect(data.id).toEqual(ticketUpdatedData.orderId);
+});
--- REACT_MICROSERVICE/ticketing/tickets/src/index.ts
+++ REACT_MICROSERVICE/ticketing/tickets/src/index.ts
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
-
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
  
@@ -37,7 +38,11 @@ const start = async () => {
      
         process.on('SIGINT', () =>  natsWrapper.client.close());
         process.on('SIGTERM', () => natsWrapper.client.close());
-           
+        
+        //instance of 
+        new OrderCreatedListener(natsWrapper.client).listen();
+        new OrderCancelledListener(natsWrapper.client).listen();
+
         await mongoose.connect( process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
