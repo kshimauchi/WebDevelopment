fc6879514ba1e6053f00f92b3dc13c1a132b688a
Ken Shimauchi <kshimauchi@gmail.com>
2021-10-03 19:06:41 -0700

    pub event job

--- REACT_MICROSERVICE/ticketing/expiration/src/queues/expiration-queue.ts
+++ REACT_MICROSERVICE/ticketing/expiration/src/queues/expiration-queue.ts
@@ -1,4 +1,6 @@
 import Queue from 'bull';
+import { ExpirationCompletePublisher} from '../events/publishers/expiration-complete-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 interface Payload {
     orderId: string;
@@ -14,8 +16,12 @@ const expirationQueue = new Queue<Payload>('order:expiration', {
 });
 //We will implement a better event here once we get a better publisher put together
 expirationQueue.process(async (job)=>{
-    console.log('I want to publish an expiration: completed event for orderId',
-    job.data.orderId
-    );
+    // console.log('I want to publish an expiration: completed event for orderId',
+    // job.data.orderId
+    // );
+    new ExpirationCompletePublisher(natsWrapper.client).publish({
+        //matching payload
+        orderId: job.data.orderId,
+    });
 });
 export {expirationQueue };
