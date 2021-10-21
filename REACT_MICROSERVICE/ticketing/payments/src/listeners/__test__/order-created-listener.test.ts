bf2ff8c46623b8dd1d00e4345d05d9673fe146b5
Ken Shimauchi <kshimauchi@gmail.com>
2021-10-21 16:46:12 -0700

    quick tests on payment setup

---
+++ REACT_MICROSERVICE/ticketing/payments/src/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,45 @@
+import { natsWrapper } from "../../nats-wrapper";
+import { OrderCreatedListener } from "../order-created-listener";
+import { OrderCreatedEvent, OrderStatus } from "@ticket-share/common";
+import mongoose from 'mongoose';
+import { Order } from "../../models/order";
+
+
+const setup = async()=>{
+    const listener = new OrderCreatedListener(natsWrapper.client);
+
+    const data: OrderCreatedEvent['data'] = {
+        id: mongoose.Types.ObjectId().toHexString(),
+        version: 0,
+        expiresAt: 'blahblah',
+        userId: 'blahblah',
+        status: OrderStatus.Created,
+        ticket: {
+            id: 'blahblah',
+            price: 10
+        }
+    };
+    //@ts-ignore
+    const msg: Message = {
+        ack: jest.fn()
+    };
+    return { listener, data, msg };
+};
+
+it('replicates the order info', async()=>{
+    const {listener, data, msg} = await setup();
+    
+    await listener.onMessage(data, msg);
+    
+    const order = await Order.findById(data.id);
+    
+    expect(order!.price).toEqual(data.ticket.price)
+});
+
+it('acks the message', async()=>{
+    const {listener, data, msg} = await setup();
+    
+    await listener.onMessage(data, msg);
+
+    expect(msg.ack).toHaveBeenCalled();
+});
