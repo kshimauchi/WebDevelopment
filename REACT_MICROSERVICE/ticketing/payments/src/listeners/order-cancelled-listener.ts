510a94c4461e6e0a7da1e403f13b6c1634a59e30
Ken Shimauchi <kshimauchi@gmail.com>
2021-10-21 17:46:36 -0700

    order cancelled listener, for making an order cancelled

---
+++ REACT_MICROSERVICE/ticketing/payments/src/listeners/order-cancelled-listener.ts
@@ -0,0 +1,24 @@
+import { OrderCancelledEvent, Subjects, Listener, OrderStatus } from "@ticket-share/common";
+import { Order } from "../models/order";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from 'node-nats-streaming';
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+    subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+    queueGroupName = queueGroupName;
+    
+    async onMessage(data: OrderCancelledEvent['data'], msg: Message){
+        const order = await Order.findOne({
+            //we will include version in case an order is updated at sometime
+            _id: data.id,
+            version: data.version-1
+        });
+        if(!order){
+            throw new Error('Order not found');
+        }
+        order.set({status: OrderStatus.Cancelled});
+        await order.save();
+
+        msg.ack();
+    }
+}
