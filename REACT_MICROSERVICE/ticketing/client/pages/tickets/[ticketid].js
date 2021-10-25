04fd1ef5b55e3cc87f972be012b7b836c12b4109
Ken Shimauchi <kshimauchi@gmail.com>
2021-10-25 13:49:33 -0700

    purchased ticket order timer display, using wild routing

--- REACT_MICROSERVICE/ticketing/client/pages/tickets/[ticketid].js
+++ REACT_MICROSERVICE/ticketing/client/pages/tickets/[ticketid].js
@@ -1,11 +1,24 @@
-
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
 //Wild Card Route
 const TicketShow = ({ticket})=>{
+    const { doRequest, errors } = useRequest({
+        url: '/api/orders',
+        method: 'post',
+        body: {
+            ticketId: ticket.id
+        },
+        onSuccess: (order)=> Router.push('/orders/[orderId]',`/orders/${order.id}`),
+    });
     return ( <div> 
         <h1>TicketShow</h1>
             <h1>{ticket.title}</h1>
             <h4>Price: {ticket.price}</h4>
-            <button className="btn btn-primary">Purchase</button>
+            {errors}
+            <button 
+                onClick={doRequest}
+                className="btn btn-primary">Purchase
+            </button>
         </div>
     );
 };
