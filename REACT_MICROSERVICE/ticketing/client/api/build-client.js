f7b5f39862c31c371a025da65bf8863834d4fd77
Ken Shimauchi <kshimauchi@gmail.com>
2021-10-25 10:40:08 -0700

    create a ticket ui, skaffold form

--- REACT_MICROSERVICE/ticketing/client/api/build-client.js
+++ REACT_MICROSERVICE/ticketing/client/api/build-client.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on the server
-
     return axios.create({
       baseURL:
         'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
