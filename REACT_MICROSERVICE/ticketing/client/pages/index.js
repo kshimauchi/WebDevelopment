1e48fbc3b53eafe12206e40fd13b245810e3bb01
Ken Shimauchi <kshimauchi@gmail.com>
2021-10-25 11:29:51 -0700

    List all tickets on previous landing page

--- REACT_MICROSERVICE/ticketing/client/pages/index.js
+++ REACT_MICROSERVICE/ticketing/client/pages/index.js
@@ -1,14 +1,40 @@
-const LandingPage = ({ currentUser }) => {
+const LandingPage = ({ currentUser , tickets}) => {
+   console.log(tickets); 
+  //Will think of a way to generate table with some filters
+  
+  
+  const ticketList = tickets.map((ticket) => {
+    return(
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
   return (
-    currentUser ? <h1>You are signed in</h1> 
-      : <h1>You are not signed in</h1>
-  );
+  <div>
+    <h1>Tickets</h1> 
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {ticketList}
+      </tbody>
+    </table>
+  </div>
+  )
 };
-//build client then make request, buildClient in api, adding client and currentUser without doing
-//a follow up with the previous version of this code
+// Refactoring 
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  //refactoring due to double fetch on landing page / app
-  return {};
+  //axios will return a data object 
+  const { data} = await client.get('/api/tickets');
+  //This will be merged into props
+  return {tickets: data}
+  
 };
 
 export default LandingPage;
@@ -16,4 +42,10 @@ export default LandingPage;
 const client = buildClient(context);
 const { data } = await client.get('/api/users/currentuser');
 return data;
+
+
+  return (
+    currentUser ? <h1>You are signed in</h1> 
+      : <h1>You are not signed in</h1>
+  );
 */
