ff410bd8e254ce44fa86e64a4c9abc136c9cc9e0
Ken Shimauchi <kshimauchi@gmail.com>
2021-10-26 14:57:15 -0700

    tickets.ts, filtering only available tickets rather than all

--- REACT_MICROSERVICE/ticketing/tickets/src/routes/index.ts
+++ REACT_MICROSERVICE/ticketing/tickets/src/routes/index.ts
@@ -5,7 +5,11 @@ const router = express.Router();
 
 router.get('/api/tickets', async (req: Request, res: Response) => {
     //Empty object for filtering or for all tickets
-    const tickets = await Ticket.find({});
+    //(1) updating to only show available to tickets
+    //(2) only tickets which do not have an order
+    const tickets = await Ticket.find({
+        orderId: undefined
+    });
     //Send it back
     res.send(tickets);
 });
