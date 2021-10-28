eda01821cbb1b9781a2f423cb889cac34afc9f10
Ken Shimauchi <kshimauchi@gmail.com>
2021-10-28 15:04:17 -0700

    broke a test

--- REACT_MICROSERVICE/ticketing/auth/src/routes/__test__/current-user.test.ts
+++ REACT_MICROSERVICE/ticketing/auth/src/routes/__test__/current-user.test.ts
@@ -10,7 +10,7 @@ it('responds with details about current user', async () => {
         .get('/api/users/currentuser')
         .set('Cookie', cookie)
         .send()
-        .expect(200);
+        .expect(400);
     expect(response.body.currentUser.email).toEqual(
         'test@test.com'
     )
