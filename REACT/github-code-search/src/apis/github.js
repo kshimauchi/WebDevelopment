e46ff285ce3fe9ec39fab88deb90803820d820ae
unknown <kshimauchi@gmail.com>
2022-05-27 14:48:28 -0800

    code-search-update

---
+++ REACT/github-code-search/src/apis/github.js
@@ -0,0 +1,13 @@
+// import axios from 'axios';
+
+// export default.axios.create(
+//     {
+//         baseURL: `${process.env.REACT_APP_URI}`,
+//         params: {
+//             maxResults: 5,
+//             key: `${process.env.REACT_APP_GITHUB_API_KEY}`,
+//             q: ''
+        
+//         },
+//     },
+// );
--- REACT/github-code-search/src/components/App.js
+++ REACT/github-code-search/src/components/App.js
@@ -1,22 +1,13 @@
+import React, {useState, useEffect, Component} from 'react';
 import './App.css';
-
-
-function displayInitialPage() {
-  return(
-    <div>
-      <h1>You are at the landing page...</h1>
-    </div>
-  );
-}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        { displayInitialPage() }
+        { `"Greetings..."` }
       </header>
     </div>
   );
 }
-
 export default App;
