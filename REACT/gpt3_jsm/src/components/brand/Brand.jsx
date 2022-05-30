e80d795a6bbec857fcc6b1c0b0c9eb81742b69fd
unknown <kshimauchi@gmail.com>
2022-05-30 15:54:14 -0800

    recommit

--- REACT/gpt3_jsm/src/components/brand/Brand.jsx
+++ REACT/gpt3_jsm/src/components/brand/Brand.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
 import './brand.css';
-const Brand = () => {
-  return (
+
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
     <div>
-        Article
+      <img src={google} alt="google" />
     </div>
-  )
-}
-export default Brand
+    <div>
+      <img src={slack} alt="slack" />
+    </div>
+    <div>
+      <img src={atlassian} alt="atlassian" />
+    </div>
+    <div>
+      <img src={dropbox} alt="dropbox"/>
+    </div>
+    <div>
+      <img src={shopify} alt="shopify"/>
+    </div>
+  </div>
+);
+
+export default Brand;
