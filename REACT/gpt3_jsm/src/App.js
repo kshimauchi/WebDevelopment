a9eed2d0031148ee6d205651b38dbcae67edbec1
unknown <kshimauchi@gmail.com>
2022-05-29 16:08:51 -0800

    folder structure, need update for react v18

---
+++ REACT/gpt3_jsm/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { Blog, Footer, Possibility, Features, WhatGPT3, Header} from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+const App = () => {
+  return (
+    <div className= "App">
+      <div className="gradient_bg">
+
+        <Navbar />
+        <Header />
+
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility/>
+      <CTA />
+      <Blog />
+      <Footer />
+
+
+    </div>
+  )
+}
+
+export default App
