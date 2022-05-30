3268a0fbbb50128b2d7e5d873771acc4132eeba9
unknown <kshimauchi@gmail.com>
2022-05-29 19:10:11 -0800

    navbar working on mobile

--- REACT/gpt3_jsm/src/components/navbar/Navbar.jsx
+++ REACT/gpt3_jsm/src/components/navbar/Navbar.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import { RiMenu3Line, RiCloseLin} from 'react-icons/ri';
+import React, {useState} from 'react';
+
+import { RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 
 import './navbar.css';
 //BEM -> block element modifier\
 
+const Menu =()=> (
+  <>
+    <p><a href="#home">Home</a></p>
+    <p><a href="#wgpt3">What is GPT3</a></p>
+    <p><a href="#possibility">Open AI</a></p>
+    <p><a href="#features">Case Studies</a></p>
+    <p><a href="#blog">Libary</a></p>
+  </>
+)
+
+
 function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false);  
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
@@ -13,19 +26,30 @@ function Navbar() {
           <img src={logo} alt="logo"/>
         </div>
         <div className="gpt3__navbar-links_container">
-          <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT3</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Libary</a></p>
+        <Menu />
         </div>
       </div>
       <div className="gpt3__navbar-sign">
         <p>Sign in</p>
         <button type="button">Sign Up</button>
       </div>
+      <div className="gpt3__navbar-menu">
+      {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+         <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+          </div>
+            <Menu />
+            <div className="gpt3__navbar_menu_container-links-sign">
+            <p>Sign in</p>
+            <button type="button">Sign Up</button>
+         </div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
-
 export default Navbar
